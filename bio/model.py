1854f550b5e8df3bdcf450a33805e2e465a1dd3c
coldtomatojuice <wall.kim@sk.com>
2020-09-30 23:44:46 +0900

    no message

--- bio/model.py
+++ bio/model.py
@@ -8,6 +8,76 @@ from dataloader import DataLoaderFinetune
 from torch_scatter import scatter_add
 from torch_geometric.nn.inits import glorot, zeros
 
+class GSANConv(MessagePassing):
+    """
+    Extension of Graph Self Attended Network aggregation to incorporate edge information by concatenation.
+
+    Args:
+        emb_dim (int): dimensionality of embeddings for nodes and edges.
+        input_layer (bool): whethe the GIN conv is applied to input layer or not. (Input node labels are uniform...)
+
+    See https://arxiv.org/abs/1810.00826
+    """
+    def __init__(self, emb_dim, aggr = "add", input_layer = False, heads=2, negative_slope=0.2):
+        super(GSANConv, self).__init__()
+        # multi-layer perceptron
+        self.mlp = torch.nn.Sequential(torch.nn.Linear(2*emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, emb_dim))
+
+        ### Mapping 0/1 edge features to embedding
+        self.edge_encoder = torch.nn.Linear(9, heads * emb_dim)
+
+        ### Mapping uniform input features to embedding.
+        self.input_layer = input_layer
+        if self.input_layer:
+            self.input_node_embeddings = torch.nn.Embedding(2, emb_dim)
+            torch.nn.init.xavier_uniform_(self.input_node_embeddings.weight.data)
+
+        self.aggr = aggr
+
+        self.emb_dim = emb_dim
+        self.heads = heads
+        self.negative_slope = negative_slope
+
+        self.weight_linear = torch.nn.Linear(emb_dim, heads * emb_dim)
+
+        self.att = torch.nn.Parameter(torch.Tensor(1, heads, 2 * emb_dim))
+        self.bias = torch.nn.Parameter(torch.Tensor(emb_dim))
+        glorot(self.att)
+        zeros(self.bias)
+
+    def forward(self, x, edge_index, edge_attr):
+        #add self loops in the edge space
+        edge_index = add_self_loops(edge_index, num_nodes = x.size(0))
+
+        #add features corresponding to self-loop edges.
+        self_loop_attr = torch.zeros(x.size(0), 9)
+        self_loop_attr[:,7] = 1 # attribute for self-loop edge
+        self_loop_attr = self_loop_attr.to(edge_attr.device).to(edge_attr.dtype)
+        edge_attr = torch.cat((edge_attr, self_loop_attr), dim = 0)
+
+        edge_embeddings = self.edge_encoder(edge_attr)
+
+        if self.input_layer:
+            x = self.input_node_embeddings(x.to(torch.int64).view(-1,))
+
+        return self.propagate(self.aggr, edge_index, x=x, edge_attr=edge_embeddings)
+
+    def message(self, edge_index, x_i, x_j, edge_attr):
+        
+        edge_attr = edge_attr.view(-1, self.heads, self.emb_dim)
+        x_j += edge_attr
+        x_j = torch.cat([x_j, edge_attr], dim = 1)
+
+        alpha = (torch.cat([x_i, x_j], dim=-1) * self.att).sum(dim=-1)
+
+        alpha = F.leaky_relu(alpha, self.negative_slope)
+        alpha = softmax(alpha, edge_index[0])
+
+        return x_j * alpha.view(-1, self.heads, 1)
+
+    def update(self, aggr_out):
+        return self.mlp(aggr_out)
+
 class GINConv(MessagePassing):
     """
     Extension of GIN aggregation to incorporate edge information by concatenation.
